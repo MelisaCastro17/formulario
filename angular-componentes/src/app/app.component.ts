c92b15ef321af66dd09d51f887e9f314b656c7bc
MelisaCastro17 <52536973+MelisaCastro17@users.noreply.github.com>
2019-10-12 19:39:48 -0500

    primer commit de trabajo

---
+++ angular-componentes/src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { CdkTextareaAutosize } from '@angular/cdk/text-field';
+import { Component, NgZone, ViewChild } from '@angular/core';
+import { take } from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  selected = 'option3';
+  favoriteSeason: string;
+  seasons: string[] = [
+    'SI',
+    'NO',
+    ];
+  formatLabel(value: number | null) {
+    if (!value) {
+      return 0;
+    }
+
+    if (value >= 1000) {
+      return Math.round(value / 1000) + 'k';
+    
+    }
+
+    return value;
+  }
+  myControl = new FormControl();
+      options: string[] = ['Pregrado', 'Potsgrado', 'Especializacion'];
+  constructor(private _ngZone: NgZone) { }
+
+  @ViewChild('autosize', { static: false }) autosize: CdkTextareaAutosize;
+
+  triggerResize() {
+    // Wait for changes to be applied, then trigger textarea resize.
+    this._ngZone.onStable.pipe(take(1))
+      .subscribe(() => this.autosize.resizeToFitContent(true));
+  }
+  color = 'accent';
+  checked = false;
+  disabled = false;
+}
+

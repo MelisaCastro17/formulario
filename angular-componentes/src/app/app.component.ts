63c254995a62ab82dab4e2cd19e41c901d9a26d3
Andres Carrero <andres_car098@hotmail.com>
2019-10-16 18:20:34 -0500

    Avance en el formulario

--- angular-componentes/src/app/app.component.ts
+++ angular-componentes/src/app/app.component.ts
@@ -1,45 +1,45 @@
-import { CdkTextareaAutosize } from '@angular/cdk/text-field';
-import { Component, NgZone, ViewChild } from '@angular/core';
-import { take } from 'rxjs/operators';
-import { FormControl } from '@angular/forms';
+import { CdkTextareaAutosize } from "@angular/cdk/text-field";
+import { Component, NgZone, ViewChild } from "@angular/core";
+import { take } from "rxjs/operators";
+import { FormControl } from "@angular/forms";
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
+
+
 export class AppComponent {
-  selected = 'option3';
+
+  constructor(private _ngZone: NgZone) {}
+  
+  myControl = new FormControl();
+  
+  selected = "option3";
   favoriteSeason: string;
-  seasons: string[] = [
-    'SI',
-    'NO',
-    ];
+  seasons: string[] = ["SI", "NO"];
+
+  options: string[] = ["Pregrado", "Potsgrado", "Especializacion"];
+
+  color = "accent";
+  checked = false;
+  disabled = false;
+
   formatLabel(value: number | null) {
     if (!value) {
       return 0;
     }
-
     if (value >= 1000) {
-      return Math.round(value / 1000) + 'k';
-    
+      return Math.round(value / 1000) + "k";
     }
-
     return value;
   }
-  myControl = new FormControl();
-      options: string[] = ['Pregrado', 'Potsgrado', 'Especializacion'];
-  constructor(private _ngZone: NgZone) { }
 
-  @ViewChild('autosize', { static: false }) autosize: CdkTextareaAutosize;
+  @ViewChild("autosize", { static: false }) autosize: CdkTextareaAutosize;
 
   triggerResize() {
-    // Wait for changes to be applied, then trigger textarea resize.
-    this._ngZone.onStable.pipe(take(1))
-      .subscribe(() => this.autosize.resizeToFitContent(true));
+    this._ngZone.onStable.pipe(take(1)).subscribe(() => this.autosize.resizeToFitContent(true));
   }
-  color = 'accent';
-  checked = false;
-  disabled = false;
 }
-
